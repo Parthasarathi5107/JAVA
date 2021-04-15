908b9efdd388565cb905d7a8ebf3b9c6a54e237d
Parthasarathi5107 <81621505+Parthasarathi5107@users.noreply.github.com>
2021-04-15 08:14:37 +0530

    Add files via upload

---
+++ p4/FriendlyCountry.java
@@ -0,0 +1,16 @@
+package p4;
+
+public class FriendlyCountry {
+
+	
+	public String countries[] =
+		{
+				"UAE",
+				"Nepal",
+				"Bhutan",
+				"Mauritius",
+				"Afganistan",
+				"Mangolia",
+		};
+	
+}
